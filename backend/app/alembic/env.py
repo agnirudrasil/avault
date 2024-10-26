a0be7b3cb511025ebcdf29245b897f560c6f0af1
Agnirudra Sil <agnirudra.sil@gmail.com>
2024-10-26 15:58:24 +0530

    Updated Dependencies

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -32,8 +32,8 @@ target_metadata = Base.metadata
 
 
 def get_url():
-    user = os.getenv("POSTGRES_USER", "postgres")
-    password = os.getenv("POSTGRES_PASSWORD", "postgres")
+    user = os.getenv("POSTGRES_USER", "agnirudrasil")
+    password = os.getenv("POSTGRES_PASSWORD", "")
     server = os.getenv("POSTGRES_HOST", "localhost:5432")
     db = os.getenv("POSTGRES_DB", "avault")
     return f"postgresql://{user}:{password}@{server}/{db}"
