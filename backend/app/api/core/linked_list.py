ff7407cd5df86d551fe315a41cd330943d97bdd7
Jaicharan Yeluri <151503362+JCode16@users.noreply.github.com>
2024-11-01 18:12:23 +0530

    Documentation linked_list.py

--- backend/app/api/core/linked_list.py
+++ backend/app/api/core/linked_list.py
@@ -1,4 +1,19 @@
 class LinkedListNode:
+    """
+    Implementing a singly linked list for the search queue.
+    """
+
     def __init__(self, value):
+        """
+        Initializes a LinkedListNode with a specified value.
+
+        Parameters
+        ----------
+        value: Any
+            The data to be stored in the node.
+        self.next: LinkedListNode or None
+            The reference to the next node in the linked list.
+            If there are no nodes next, self.next is set to None.
+        """
         self.value = value
         self.next = None
