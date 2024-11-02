5d03542f3d0f755c343126a75df2f6d66de875ae
JCode16 <jai.charan944@gmail.com>
2024-11-02 15:37:50 +0530

    sphinx docs updated

--- backend/app/api/core/linked_list.py
+++ backend/app/api/core/linked_list.py
@@ -1,4 +1,27 @@
 class LinkedListNode:
+    """
+    A class representing a node in a singly linked list.
+
+    Attributes
+    ----------
+    value : Any
+        The value stored in the node.
+    next : LinkedListNode or None
+        A reference to the next node in the linked list. Defaults to None.
+
+    Methods
+    -------
+    __init__(value):
+        Initializes a LinkedListNode with the given value and sets the next node reference to None.
+    """
     def __init__(self, value):
+        """
+        Initializes a new LinkedListNode.
+
+        Parameters
+        ----------
+        value : Any
+            The value to be stored in the node.
+        """
         self.value = value
         self.next = None
